6de3df204728bb2eeb2de11442028e91c8abeda3
nhardy27 <neelprajapati229@gmail.com>
2026-02-19 10:16:04 +0530

    Remove employee folder

--- api/Department/model.py
+++ api/Department/model.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 class Department(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, null=True,blank=True)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
