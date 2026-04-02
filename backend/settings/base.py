6700191b4bbcca56e4a5cbcc268b674a8345cfa9
nhardy27 <neelprajapati229@gmail.com>
2026-04-02 17:53:29 +0530

    base py

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -195,17 +195,7 @@ CORS_ALLOW_METHODS = [
     'PUT',
 ]
 
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
+CORS_ALLOW_HEADERS = ['*']
 
 
 
